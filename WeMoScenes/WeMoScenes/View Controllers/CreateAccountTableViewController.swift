fb385e1fbd4861ab3afbff7decaca9be8e95d172
Kevin Voell <KevinVoell@users.noreply.github.com>
2016-06-28 09:39:13 -0600

    Added code to allow account merge

--- WeMoScenes/WeMoScenes/View Controllers/CreateAccountTableViewController.swift
+++ WeMoScenes/WeMoScenes/View Controllers/CreateAccountTableViewController.swift
@@ -16,7 +16,9 @@ class CreateAccountTableViewController: UITableViewController {
   @IBOutlet weak var confirmPasswordTextfield: UITextField!
   
   @IBAction func createAccountTapped(sender: AnyObject) {
-    if passwordTextField.text! != confirmPasswordTextfield.text! {
+    if emailTextField.text! == "" {
+      self.showAlert("Error", "Please enter an Email Address")
+    } else if passwordTextField.text! != confirmPasswordTextfield.text! {
       let alert = UIAlertController(title: "Error", message: "Passwords must match, please try again.", preferredStyle: .Alert)
       
       alert.addAction(UIAlertAction(title: "Dismiss", style: .Cancel, handler: nil))
@@ -25,26 +27,35 @@ class CreateAccountTableViewController: UITableViewController {
         self.passwordTextField.text = ""
         self.confirmPasswordTextfield.text = ""
       })
-      
     } else {
-      FIRAuth.auth()?.createUserWithEmail(emailTextField.text!, password: passwordTextField.text!, completion: { (user, error) in
+      if FIRAuth.auth()?.currentUser != nil && FIRAuth.auth()?.currentUser.anonymous {
+        // Link account
+        let credential = FIREmailPasswordAuthProvider.credentialWithEmail(emailTextField.text!, password: passwordTextField.text!)
+        
+        FIRAuth.auth()?.currentUser.linkWithCredential(credential, completion: handleAccountCreation) 
+      } else {
+        // Create account
+        FIRAuth.auth()?.createUserWithEmail(emailTextField.text!, password: passwordTextField.text!, completion: handleAccountCreation)
+      }
+    }
+  }
+  
+  func handleAccountCreation((user, error)) {
+    if error != nil {
+      let alert = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .Alert)
+      
+      alert.addAction(UIAlertAction(title: "Dismiss", style: .Cancel, handler: nil))
+      
+      self.presentViewController(alert, animated: true, completion: {
+        self.passwordTextField.text = ""
+        self.confirmPasswordTextfield.text = ""
+      })
+    } else {
+      user?.sendEmailVerificationWithCompletion({ (error) in
         if error != nil {
-          let alert = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .Alert)
-          
-          alert.addAction(UIAlertAction(title: "Dismiss", style: .Cancel, handler: nil))
-          
-          self.presentViewController(alert, animated: true, completion: {
-            self.passwordTextField.text = ""
-            self.confirmPasswordTextfield.text = ""
-          })
+          self.showAlert("Error", message: error!.localizedDescription)
         } else {
-          user?.sendEmailVerificationWithCompletion({ (error) in
-            if error != nil {
-              self.showAlert("Error", message: error!.localizedDescription)
-            } else {
-              self.showAlert("Confirm Email", message: "Check your email to confirm your account.")
-            }
-          })
+          self.showAlert("Confirm Email", message: "Check your email to confirm your account.")
         }
       })
     }
